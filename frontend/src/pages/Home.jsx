996114d1504053ee6f5ec0386d6668e1253c1a8c
Tipunen <topitimonen01@gmail.com>
2023-09-13 11:18:44 +0300

    PAGENAMES

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,16 @@ const HomePage = () => {
   return (
     <div>
       {/* <Navigate to="/survey" /> */}
-      <h1>Home</h1>
+      <h1>Log in User</h1>
+      <p>Visual result Page,
+        Property Manager Log in,
+        PM Main Pnpm age,
+        PM Make survey ,
+        PM Previev Survey,
+        PM Your Surveys,
+        PM View/Edit Survey,
+        PM Survey Results
+      </p>
       <Link to="/survey">Survey</Link>
     </div>
   );
