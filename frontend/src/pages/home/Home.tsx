88239b1eba8781f6f334193f25e488522e94e6f3
Tatu <tatu.d.ihaksi@gmail.com>
2023-09-24 12:22:37 +0300

    home page elements

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,27 +1,46 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
+import { useState } from 'react';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const [surveyKey, setSurveyKey] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate('/survey');
+  };
+
   return (
     <div style={{ padding: '2em' }}>
-      {/* <Navigate to="/survey" /> */}
-      <h1>Home page</h1>
-      <p>
-        Visual result Page, Property Manager Log in, PM Main Pnpm age, PM Make
-        survey , PM Previev Survey, PM Your Surveys, PM View/Edit Survey, PM
-        Survey Results
-      </p>
-      <div className={styles.test_box}></div>
-      <div className="column">
+      <div className={styles.main}>
+        <h1>Home page</h1>
+        <h1>HIMA</h1>
+        <h3>Hiilijalanjäljen Minimointi Asuinkiinteistöissä</h3>
+        <h4>Pääsykoodi</h4>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            required
+            placeholder="Pääsykoodi"
+            onChange={(e) => setSurveyKey(e.target.value)}
+          />
+          <button>Siirry kyselyyn</button>
+        </form>
+      </div>
+
+      <Link to="/login">Kirjaudu/Login</Link>
+
+      {/* <div className="column">
         <Link to="/survey">Survey answer</Link>
         <Link to="/survey/edit">Edit Survey</Link>
         <Link to="/create">Create Survey</Link>
         <Link to="/login">Login</Link>
         <Link to="/surveys">Surveys</Link>
-        {/* <Link to="/preview">Preview</Link> */}
+        <Link to="/preview">Preview</Link>
         <Link to="/properties">Properties</Link>
         <Link to="/user-results">User Results</Link>
-      </div>
+      </div> */}
     </div>
   );
 };
