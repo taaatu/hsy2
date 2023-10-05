8c81fb0f8458a6da59ed894b9c0bb706c71d7500
Tatu <tatu.d.ihaksi@gmail.com>
2023-10-05 15:06:10 +0300

    small fix

--- frontend/src/AdminRoutes.tsx
+++ frontend/src/AdminRoutes.tsx
@@ -8,7 +8,7 @@ import { UserGroup } from './interfaces/User';
 // https://medium.com/@dennisivy/creating-protected-routes-with-react-router-v6-2c4bbaf7bc1c
 const AdminRoutes = () => {
   const { curentUser } = useContext(MainContext);
-  return TOKEN && curentUser?.user_group === UserGroup.ADMIN ? (
+  return curentUser?.user_group === UserGroup.ADMIN ? (
     <>
       <TopNavBar isAdmin={true} />
       <Outlet />
--- frontend/src/PropertyManagerRoutes.tsx
+++ frontend/src/PropertyManagerRoutes.tsx
@@ -1,9 +1,12 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import { TopNavBar } from './components/TopNavBar';
 import { TOKEN } from './variables/Constants';
+import { useContext } from 'react';
+import { MainContext } from './context/MainContext';
 
 export const PropertyManagerRoutes = () => {
-  return TOKEN ? (
+  const { curentUser } = useContext(MainContext);
+  return curentUser ? (
     <>
       <TopNavBar isAdmin={false} />
       <Outlet />
