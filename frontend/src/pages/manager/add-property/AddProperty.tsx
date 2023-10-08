a01e5e3e7ba66e1bd65ff4c1da97f879094d540d
Tatu <tatu.d.ihaksi@gmail.com>
2023-10-08 08:14:01 +0300

    add property form collect input

--- frontend/src/pages/manager/add-property/AddProperty.tsx
+++ frontend/src/pages/manager/add-property/AddProperty.tsx
@@ -1,22 +1,60 @@
+import { useState } from 'react';
 import { CITIES } from '../../../variables/Constants';
+import { Property } from '../../../interfaces/Property';
+import useProperty from '../../../hooks/PropertyHook';
 
 export const AddProperty = () => {
+  const { addProperty } = useProperty();
+  const [property, setProperty] = useState<Partial<Property>>({
+    city: CITIES[0],
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setProperty({
+      ...property,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await addProperty(property as Property);
+  };
+
   return (
     <div className="centered-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h4>Taloyhtiö</h4>
         <label>
           Osoite
-          <input placeholder="Osoite" />
+          <input
+            required
+            placeholder="Osoite"
+            name="address"
+            onChange={handleChange}
+          />
         </label>
         <label>
           Postinumero
-          <input placeholder="Postinumero" />
+          <input
+            placeholder="Postinumero"
+            required
+            name="postcode"
+            onChange={handleChange}
+          />
         </label>
 
         <label>
           Kaupunki
-          <select>
+          <select
+            name="city"
+            required
+            onChange={handleChange}
+            defaultValue={property.city}
+          >
             {CITIES.map((city: string) => (
               <option key={city}>{city}</option>
             ))}
@@ -25,7 +63,12 @@ export const AddProperty = () => {
 
         <label>
           Taloyhtiön nimi
-          <input placeholder="Taloyhtiön nimi" />
+          <input
+            required
+            placeholder="Taloyhtiön nimi"
+            name="name"
+            onChange={handleChange}
+          />
         </label>
 
         <button>Lisää taloyhtiö</button>
