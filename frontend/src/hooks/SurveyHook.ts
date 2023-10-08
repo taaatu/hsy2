31b1dc56e786c45696534129f58d6ff7bbce8be4
Tatu <tatu.d.ihaksi@gmail.com>
2023-10-08 08:20:35 +0300

    surveys page prepared for api call

--- frontend/src/hooks/SurveyHook.ts
+++ frontend/src/hooks/SurveyHook.ts
@@ -1,16 +1,33 @@
+import { MessageResponse } from '../interfaces/Response';
 import { Survey } from '../interfaces/Survey';
 import { doFetch } from './DoFetch';
 
 const useSurvey = () => {
-  const createSurvey = async (survey: Survey) => {
+  const getSurveys = async () => {
     try {
-      const response = await doFetch('survey', 'POST', survey);
-      return response;
+      // const response = (await doFetch('survey', 'GET')) as Survey[];
+      // console.log(response);
+      // return response;
     } catch (error: any) {
       throw new Error(error.message);
     }
   };
-  return { createSurvey };
+  const createSurvey = async (survey: Survey) => {
+    try {
+      // const response = (await doFetch(
+      //   'survey',
+      //   'POST',
+      //   survey
+      // )) as MessageResponse;
+      // console.log(response);
+      alert('Kysely luotu');
+    } catch (error: any) {
+      alert('Kyselyn luominen epäonnistui');
+      throw new Error(error.message);
+    }
+  };
+
+  return { getSurveys, createSurvey };
 };
 
 export default useSurvey;
--- frontend/src/pages/admin/surveys/SurveysPage.tsx
+++ frontend/src/pages/admin/surveys/SurveysPage.tsx
@@ -1,9 +1,47 @@
 // Page that displays all surveys
+import { useEffect, useState } from 'react';
+import useSurvey from '../../../hooks/SurveyHook';
+import { Survey } from '../../../interfaces/Survey';
 
 const SurveysPage = () => {
+  const { getSurveys } = useSurvey();
+  const [surveys, setSurveys] = useState<Survey[]>([]);
+  const [fullList, setFullList] = useState<Survey[]>([]);
+
+  const fetchSurveys = async () => {
+    // const surveys = await getSurveys();
+    // console.log('Surveys: ', surveys);
+    // if (!surveys) return;
+    // setSurveys(surveys);
+    // setFullList(surveys);
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const search = e.target.value;
+    const filteredSurveys = fullList.filter(
+      (survey) => survey.title.toLowerCase().includes(search.toLowerCase()) //||
+      // user.company.toLowerCase().includes(search.toLowerCase())
+    );
+    setSurveys(filteredSurveys);
+  };
+
+  useEffect(() => {
+    fetchSurveys();
+  }, []);
+
   return (
     <div>
       <h1>Surveys Page</h1>
+      <label>
+        <input onChange={handleSearch} type="text" />
+      </label>
+
+      <h4>{`Kyselyt (${surveys.length})`}</h4>
+      {surveys.map((survey) => (
+        <div>
+          <h3>{survey.title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
