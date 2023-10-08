2f49873676e0a2e47093a45b9aa367639fc5c198
Xiaoming Ma <73438688+myxmxm@users.noreply.github.com>
2023-10-08 11:33:53 +0300

    get list of survey, get survey detail by survey id, delete survey by survey id and post survey are ready

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const cors = require('cors');
 const authRoute = require('./routes/authRoute');
 const userRoute = require('./routes/userRoute');
+const surveyRoute = require('./routes/surveyRoute');
 const { httpError } = require('./utils/errors');
 const passport = require('./utils/pass');
 const app = express()
@@ -14,16 +15,17 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/auth', authRoute);
-app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
+app.use('/user', passport.authenticate('jwt', { session: false }), userRoute);
+app.use('/survey', passport.authenticate('jwt', {session: false}), surveyRoute);
 
 app.use((req, res, next)=>{
     const err = httpError('Not found', 404);
     next(err);
-  });
+});
   
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    res.status(status).json({message: err.message || "internal error"});
+    const statusCode = err.status || 400;
+    res.status(statusCode).json({ message: err.message, status: statusCode });
 });
 
 app.listen(port, () => {
