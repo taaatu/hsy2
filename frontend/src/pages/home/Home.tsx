59b4d2d27477ec945327a71ea1c37672fdb7dcf0
Tatu <tatu.d.ihaksi@gmail.com>
2023-10-11 23:28:04 +0300

    some styling

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import styles from './Home.module.css';
 import { useState } from 'react';
 import { AppName } from '../../components/AppName';
@@ -10,37 +10,29 @@ const HomePage = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    navigate('/survey');
+    navigate('/survey/' + surveyKey);
   };
 
   return (
     <div style={{ padding: '2em' }}>
       <div className={styles.main}>
-        <h1 >Home page</h1>
         <AppName />
-        <h4>Pääsykoodi</h4>
         <form onSubmit={handleSubmit}>
+          <h4 style={{ margin: 0 }}>Syötä kyselyn pääsykoodi</h4>
           <input
             type="text"
             required
             placeholder="Pääsykoodi"
             onChange={(e) => setSurveyKey(e.target.value)}
           />
-          <button className='button'>Siirry kyselyyn</button>
+
+          <button className="button">Siirry kyselyyn</button>
         </form>
         <HsyLogo />
       </div>
-      <Link to="/login" className='linktologin'>Kirjaudu/Login</Link>
-      {/* <div className="column">
-        <Link to="/survey">Survey answer</Link>
-        <Link to="/survey/edit">Edit Survey</Link>
-        <Link to="/create">Create Survey</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/surveys">Surveys</Link>
-        <Link to="/preview">Preview</Link>
-        <Link to="/properties">Properties</Link>
-        <Link to="/user-results">User Results</Link>
-      </div> */}
+      <Link to="/login" className="linktologin">
+        Kirjaudu/Login
+      </Link>
     </div>
   );
 };
