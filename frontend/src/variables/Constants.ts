d9f4e1a2aa97d3a1f780e561b1e8cf05f42a38c7
Tatu <tatu.d.ihaksi@gmail.com>
2023-10-13 11:43:54 +0300

    password pattern frontend

--- frontend/src/variables/Constants.ts
+++ frontend/src/variables/Constants.ts
@@ -4,3 +4,4 @@ export const ANSWER_2 = 'Asialla ei ole merkitystä tai asia ei koske minua';
 export const ANSWER_3 = 'En pidä tärkeänä tai en toimi näin';
 export const BASE_URL = 'http://localhost:3000/';
 export const TOKEN = sessionStorage.getItem('token');
+export const PASSWORD_REGEX = '(?=.*[A-Z]).{8,}';
