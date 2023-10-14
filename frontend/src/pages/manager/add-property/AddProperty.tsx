a08d855042c326b68912c53dc3c62cfdfb15faf1
Tatu <tatu.d.ihaksi@gmail.com>
2023-10-14 18:39:34 +0300

    add property form validation

--- frontend/src/pages/manager/add-property/AddProperty.tsx
+++ frontend/src/pages/manager/add-property/AddProperty.tsx
@@ -2,59 +2,48 @@ import { useState } from 'react';
 import { CITIES } from '../../../variables/Constants';
 import { Property } from '../../../interfaces/Property';
 import useProperty from '../../../hooks/PropertyHook';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { FormFieldError } from '../../../components/FormFieldError';
 
 export const AddProperty = () => {
   const { addProperty } = useProperty();
-  const [property, setProperty] = useState<Partial<Property>>({
-    city: CITIES[0],
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Property>({
+    defaultValues: { city: CITIES[0] },
   });
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const { name, value } = e.target;
-    setProperty({
-      ...property,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await addProperty(property as Property);
-  };
+  const onSubmit: SubmitHandler<Property> = async (data) => addProperty(data);
 
   return (
     <div className="centered-container">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h4>Taloyhtiö</h4>
         <label>
           Osoite
           <input
-            required
             placeholder="Osoite"
-            name="address"
-            onChange={handleChange}
+            {...register('address', { required: 'Osoite vaaditaan' })}
           />
+          <FormFieldError error={errors.address} />
         </label>
         <label>
           Postinumero
           <input
             placeholder="Postinumero"
-            required
-            name="postcode"
-            onChange={handleChange}
+            {...register('postcode', {
+              required: 'Postinumero vaaditaan',
+            })}
           />
+          <FormFieldError error={errors.postcode} />
         </label>
 
         <label>
           Kaupunki
-          <select
-            name="city"
-            required
-            onChange={handleChange}
-            defaultValue={property.city}
-          >
+          <select {...register('city', { required: true })}>
             {CITIES.map((city: string) => (
               <option key={city}>{city}</option>
             ))}
@@ -64,11 +53,10 @@ export const AddProperty = () => {
         <label>
           Taloyhtiön nimi
           <input
-            required
+            {...register('name', { required: 'Nimi vaaditaan' })}
             placeholder="Taloyhtiön nimi"
-            name="name"
-            onChange={handleChange}
           />
+          <FormFieldError error={errors.name} />
         </label>
 
         <button>Lisää taloyhtiö</button>
