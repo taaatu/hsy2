dc53aa7b0bd20ad065c173c3f9d678f2f748edb9
Tatu <tatu.d.ihaksi@gmail.com>
2023-10-17 17:44:40 +0300

    assign buildings to survey in create survey page

--- frontend/src/pages/admin/createsurvey/SelectProperties.tsx
+++ frontend/src/pages/admin/createsurvey/SelectProperties.tsx
@@ -1,15 +1,63 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CITIES } from '../../../variables/Constants';
+import styles from './CreateSurvey.module.css';
+import { Building } from '../../../interfaces/Building';
+import useBuilding from '../../../hooks/BuildingHook';
 
-enum SelectLevel {
+export enum SelectLevel {
   ALL = 'all',
   CITY = 'city',
   POSTAL_CODE = 'postal_code',
   NONE = 'none',
 }
-export const SelectProperties = () => {
-  // const [isAll, setIsAll] = useState<boolean>(true);
-  const [selectLevel, setSelectLevel] = useState<SelectLevel>(SelectLevel.ALL);
+type Props = {
+  selectLevel: SelectLevel;
+  setSelectLevel: (selectLevel: SelectLevel) => void;
+  selectedBuildings: Building[];
+  setSelectedBuildings: (buildings: Building[]) => void;
+};
+
+export const SelectProperties = ({
+  setSelectLevel,
+  selectLevel,
+  setSelectedBuildings,
+  selectedBuildings,
+}: Props) => {
+  const { getAllBuildings } = useBuilding();
+  const [buildings, setBuildings] = useState<Building[]>([]);
+
+  const addBuilding = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    const _b = buildings.find((b) => b.building_id === Number(value));
+    const duplicate = selectedBuildings.find(
+      (b) => b.building_id === Number(value)
+    );
+    if (duplicate) return;
+    if (!_b) return;
+    setSelectedBuildings([...selectedBuildings, _b]);
+    console.log('selected buildings: ', selectedBuildings);
+  };
+
+  const removeFromSelected = (id: number) => {
+    setSelectedBuildings(selectedBuildings.filter((b) => b.building_id !== id));
+  };
+
+  const onSelectAll = () => {
+    if (selectLevel === SelectLevel.ALL) {
+      setSelectedBuildings([]);
+      setSelectLevel(SelectLevel.NONE);
+      return;
+    }
+    setSelectedBuildings(buildings);
+    setSelectLevel(SelectLevel.ALL);
+  };
+
+  useEffect(() => {
+    (async () => {
+      setBuildings(await getAllBuildings());
+    })();
+  }, []);
+
   return (
     <div>
       <h2>Valitse taloyhtiöt</h2>{' '}
@@ -17,39 +65,60 @@ export const SelectProperties = () => {
         <label style={{ display: 'flex', flexDirection: 'row' }}>
           Kaikki
           <input
-            // onChange={() => setIsAll(!isAll)}
-            onChange={() =>
-              setSelectLevel(
-                selectLevel === SelectLevel.ALL
-                  ? SelectLevel.NONE
-                  : SelectLevel.ALL
-              )
-            }
-            // checked={isAll}
+            onChange={() => onSelectAll()}
             checked={selectLevel === SelectLevel.ALL}
             type="checkbox"
           />
         </label>
-        {/* {isAll ? <div>Something</div> : <div>Something else</div>} */}
-        {selectLevel !== SelectLevel.ALL && (
-          <>
+        <div style={{ position: 'relative' }}>
+          {selectLevel === SelectLevel.ALL && (
+            <div className={styles.overlay}></div>
+          )}
+          <div className="flex-row" style={{ gap: '1rem' }}>
+            <select onChange={addBuilding} style={{ height: 'fit-content' }}>
+              {buildings.map((building: Building) => (
+                <option key={building.building_id} value={building.building_id}>
+                  {building.name} {building.building_id}
+                </option>
+              ))}
+            </select>
             <div>
-              <SelectCity
-                selectLevel={selectLevel}
-                setSelectLevel={setSelectLevel}
-              />
-              <SelectPostalCode />
+              Taloyhtiöt {`(${selectedBuildings.length})`}
+              {selectedBuildings.map((building: Building) => (
+                <div key={building.building_id}>
+                  {building.name}{' '}
+                  <button
+                    onClick={() => removeFromSelected(building.building_id)}
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
             </div>
-          </>
-        )}
+          </div>
+          {/* <SelectCity
+            selectLevel={selectLevel}
+            setSelectLevel={setSelectLevel}
+          />
+          <SelectPostalCode /> */}
+        </div>
       </div>
     </div>
   );
 };
-type Props = {
-  selectLevel: SelectLevel;
-  setSelectLevel: (selectLevel: SelectLevel) => void;
+
+const SelectProperty = ({ buildings }: { buildings: Building[] }) => {
+  return (
+    <div>
+      <select>
+        {buildings.map((building: Building) => (
+          <option key={building.building_id}>{building.name}</option>
+        ))}
+      </select>
+    </div>
+  );
 };
+
 const SelectCity = ({ selectLevel, setSelectLevel }: Props) => {
   // const [isSelected, setIsSelected] = useState<boolean>(false);
   const [city, setCity] = useState<string>('');
