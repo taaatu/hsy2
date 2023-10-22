8cc529530c91b8af4b5c8560d3cb8ff55cae2787
Tatu <tatu.d.ihaksi@gmail.com>
2023-10-22 08:49:54 +0300

    answer survey form component

---
+++ frontend/src/components/forms/AnswerQuestion.tsx
@@ -0,0 +1,84 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Question } from '../../interfaces/Question';
+import { Answer } from '../../interfaces/Answer';
+
+type Props = {
+  question: Partial<Question>;
+  index: number;
+  answers: Partial<Answer>[];
+  setAnswers: Dispatch<SetStateAction<Partial<Answer>[]>>;
+  isPreview?: boolean;
+};
+
+const AnswerQuestion = ({
+  question,
+  index,
+  answers,
+  setAnswers,
+  isPreview,
+}: Props) => {
+  const [answer, setAnswer] = useState<string>();
+
+  // Update answers array whem answer is selected from radio buttons
+  const changeAnswer = (answer: string) => {
+    console.log('answer: ', answer);
+    setAnswers(
+      answers.map((a, i) => {
+        if (a.question === question.question) {
+          return { ...a, answer: answer };
+        }
+        return a;
+      })
+    );
+  };
+
+  return (
+    <>
+      <div className="column">
+        <h4>
+          {index + 1}. {question.question}
+        </h4>
+        <div>
+          <input
+            type="radio"
+            name={question.question}
+            disabled={isPreview}
+            value={question.option_1}
+            // onChange={() => setAnswer(question.answer1)}
+            onChange={() => changeAnswer(question.option_1 ?? '')}
+            // checked={answer === 1}
+            required
+          />{' '}
+          {question.option_1}
+        </div>
+        <div>
+          <input
+            type="radio"
+            name={question.question}
+            disabled={isPreview}
+            value={question.option_2}
+            // onChange={() => setAnswer(question.answer2)}
+            onChange={() => changeAnswer(question.option_2 ?? '')}
+            // checked={answer === 2}
+            required
+          />{' '}
+          {question.option_2}
+        </div>
+        <div>
+          <input
+            type="radio"
+            name={question.question}
+            disabled={isPreview}
+            value={question.option_3}
+            onChange={() => changeAnswer(question.option_3 ?? '')}
+            required
+          />{' '}
+          {question.option_3}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AnswerQuestion;
--- frontend/src/pages/resident/answer-survey/SurveyAnswerPage.tsx
+++ frontend/src/pages/resident/answer-survey/SurveyAnswerPage.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Survey } from '../../../interfaces/Survey';
-import AnswerQuestion from './AnswerQuestion';
+import AnswerQuestion from '../../../components/forms/AnswerQuestion';
 import PropTypes from 'prop-types';
 import { Answer } from '../../../interfaces/Answer';
+import { AnswerSurveyForm } from '../../../components/forms/AnswerSurveyForm';
 
 type Props = {
   survey: Partial<Survey>;
@@ -10,42 +11,9 @@ type Props = {
 };
 
 export const SurveyAnswerPage = ({ survey, isPreview = false }: Props) => {
-  const [answers, setAnswers] = useState<Partial<Answer>[]>([]);
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (isPreview) return;
-    alert('Kysely lähetetty');
-    console.log('Answers: ', answers);
-  };
-  useEffect(() => {
-    if (isPreview) return;
-    const _questions = survey.questions?.map((q) => ({
-      question: q.question,
-    })) as Partial<Answer>[];
-    console.log('s: ', _questions);
-    setAnswers(_questions);
-  }, [survey]);
   return (
-    <div className="survey-bg">
-      <h2>{survey.survey_header?.survey_title ?? 'Ei nimeä'}</h2>
-      <p>{survey.survey_header?.description ?? 'Ei kuvausta'}</p>
-      <h4>{`Kysymyksiä (${survey.questions?.length})`}</h4>
-      <form onSubmit={handleSubmit}>
-        {survey.questions !== undefined && (
-          <>
-            {survey.questions.map((question, index) => (
-              <AnswerQuestion
-                key={question.question_id}
-                question={question}
-                index={index}
-                answers={answers}
-                setAnswers={setAnswers}
-              />
-            ))}
-          </>
-        )}
-        <input type="submit" value="Lähetä" />
-      </form>
+    <div>
+      <AnswerSurveyForm survey={survey} isPreview={isPreview} />
     </div>
   );
 };
