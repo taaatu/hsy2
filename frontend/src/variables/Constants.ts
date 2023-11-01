d3d613dd4b4912454c534dfbca1cb51059b6da69
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-01 14:38:21 +0200

    add building error message and custom alert

--- frontend/src/variables/Constants.ts
+++ frontend/src/variables/Constants.ts
@@ -5,3 +5,4 @@ export const ANSWER_3 = 'En pidä tärkeänä tai en toimi näin';
 export const BASE_URL = 'http://localhost:3000/';
 export const TOKEN = sessionStorage.getItem('token');
 export const PASSWORD_REGEX = /(?=.*[A-Z]).{8,}/;
+export const POST_CODE_REGEX = /^\d{5}$/;
