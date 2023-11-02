e1894e057d99c0b36a43527d76a1ec64f9912206
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-02 15:23:55 +0200

    app name size

--- frontend/src/components/AppName.tsx
+++ frontend/src/components/AppName.tsx
@@ -1,8 +1,11 @@
+import logo from '../assets/hima.png';
+
 const AppName = () => {
   return (
     <div className="app-name">
       {/* <h1 className='hima'></h1>
       <h3></h3> */}
+      <img style={{ maxWidth: '100%' }} src={logo} />
     </div>
   );
 };
