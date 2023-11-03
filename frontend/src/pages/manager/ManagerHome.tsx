c19eabe30ce9571cab8fdcd605c1992cf6d73ccc
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-03 14:51:30 +0200

    user's home page greeting

--- frontend/src/pages/manager/ManagerHome.tsx
+++ frontend/src/pages/manager/ManagerHome.tsx
@@ -1,15 +1,9 @@
-import { useNavigate } from 'react-router-dom';
+import { WelcomeText } from '../../components/WelcomeText';
 
 const ManagerHome = () => {
-  const navigate = useNavigate();
   return (
     <div className="centered-container">
-      <h1>Property Manager home</h1>
-      <button>Kyselyt</button>
-      <button>Kiinteistöt</button>
-      <button onClick={() => navigate('/manager/addproperty')}>
-        Lisää kiinteistö
-      </button>
+      <WelcomeText />
     </div>
   );
 };
