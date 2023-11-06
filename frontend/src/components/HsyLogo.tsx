a220dd167c1e467c6575d59fe947d20f11a250bc
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-06 20:03:19 +0200

    some code cleaning

--- frontend/src/components/HsyLogo.tsx
+++ frontend/src/components/HsyLogo.tsx
@@ -2,9 +2,5 @@ import logo from '../assets/hsy_icon.webp';
 import styles from './Components.module.css';
 
 export const HsyLogo = () => {
-  return (
-    <div className={styles.hsyLogo}>
-      <img style={{ maxWidth: '80%' }} src={logo} alt="HSY" />
-    </div>
-  );
+  return <img className={styles.hsyLogo} src={logo} alt="HSY" />;
 };
