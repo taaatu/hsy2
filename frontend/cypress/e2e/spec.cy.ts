6fa0e4883767b1d53644d16f1a3c6e2f4e49fd2d
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-11 15:00:52 +0200

    some tests added

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -30,11 +30,11 @@ describe('template spec', () => {
     cy.get('input[name="full_name"]').type(testUser.full_name);
     cy.get('input[name="company"]').type(testUser.company);
     cy.get('button').click();
-    cy.get('button').contains('OK').click();
+    cy.get('#success-ok').click();
     cy.url().should('include', '/admin/managers');
   });
 
-  it('login with test user', () => {
+  it('login with test user and modify profile', () => {
     cy.visit('http://localhost:5173/login');
     cy.get('input[name="email"]').type(testUser.email);
     cy.get('input[name="password"]').type(testUser.password);
@@ -44,7 +44,29 @@ describe('template spec', () => {
     cy.get('a[href="/manager/profile"]').click();
     cy.url().should('include', '/manager/profile');
 
+    // Modify profile
+    cy.get('button').contains('Muokkaa').click();
+    testUser.full_name = 'Jack Doe';
+    testUser.email = 'jack.doe@test.com';
+    testUser.company = 'Modified company';
+    testUser.password = 'NewPassword1234';
+    cy.get('input[name="full_name"]').clear().type(testUser.full_name);
+    cy.get('input[name="email"]').clear().type(testUser.email);
+    cy.get('input[name="company"]').clear().type(testUser.company);
+    cy.get('input[name="password"]').clear().type(testUser.password);
+    cy.get('button').contains('Tallenna').click();
+  });
+
+  it('login with modified test user', () => {
+    cy.visit('http://localhost:5173/login');
+    cy.get('input[name="email"]').type(testUser.email);
+    cy.get('input[name="password"]').type(testUser.password);
+    cy.get('button').click();
+    cy.url().should('include', '/manager');
+
     // Remove test user
-    cy.get('button').contains('Poista').click();
+    cy.get('.nav-profile').click();
+    cy.get('a[href="/manager/profile"]').click();
+    cy.get('button.delete').click();
   });
 });
