dcd5bf11d17c2e56401cd431b790597958e8425f
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-16 22:52:39 +0200

    Updates

---
+++ assets/AdminHome-cc90aab3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-fd9a55c3.js";import{W as r}from"./WelcomeText-e2a66d20.js";const o=()=>e.jsx("main",{className:"centered-container",children:e.jsx(r,{})});export{o as default};
--- assets/ManagerHome-40fd13b9.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-3f9fbaf7.js";import{W as r}from"./WelcomeText-4b2796ee.js";const s=()=>e.jsx("div",{className:"centered-container",children:e.jsx(r,{})});export{s as default};
