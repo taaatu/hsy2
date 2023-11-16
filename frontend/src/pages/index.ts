e7bf6cf3a010cf5f69120a989ffa7733c80f7685
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-16 02:26:54 +0200

    something

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,59 @@
+import { lazy } from 'react';
+// Lazy load all the pages in the application
+
+export const AdminHome = lazy(() => import('./admin/AdminHome'));
+export const CreateUserPage = lazy(() =>
+  import('./admin/property-managers/CreateUserPage')
+);
+export const CreateSurveyPage = lazy(() =>
+  import('./admin/surveys/create/CreateSurveyPage')
+);
+export const EditSurveyPage = lazy(() =>
+  import('./admin/surveys/modify/EditSurveyPage')
+);
+export const PropertiesPage = lazy(() =>
+  import('./admin/properties/PropertiesPage')
+);
+export const SurveysPage = lazy(() => import('./admin/surveys/SurveysPage'));
+export { HomePage } from './home/Home';
+export const LoginPage = lazy(() => import('./login/LoginPage'));
+export const SurveyAnswerPage = lazy(() =>
+  import('./resident/SurveyAnswerPage')
+);
+export const UserResultsPage = lazy(() =>
+  import('./resident/results/UserResultsPage')
+);
+export const AddBuildingPage = lazy(() =>
+  import('./manager/buildings/AddBuildingPage')
+);
+export const PropertyManagers = lazy(() =>
+  import('./admin/property-managers/list/PropertyManagers')
+);
+export const PropertyManagerPage = lazy(() =>
+  import('./admin/property-managers/single/ProperyManagerPage')
+);
+export const ProfilePage = lazy(() => import('./manager/profile/ProfilePage'));
+export const SingleSurveyPage = lazy(() =>
+  import('./admin/surveys/SingleSurveyPage')
+);
+export const SinglePropertyPage = lazy(() =>
+  import('./admin/properties/SinglePropertyPage')
+);
+export const Logout = lazy(() => import('./Logout'));
+export const ManagerSurveysPage = lazy(() =>
+  import('./manager/surveys/ManagerSurveysPage')
+);
+export const ManagerSingleSurveyPage = lazy(() =>
+  import('./manager/surveys/ManagerSingleSurveyPage')
+);
+export const ManagerProperties = lazy(() =>
+  import('./manager/buildings/ManagerProperties')
+);
+export const ManagerHome = lazy(() => import('./manager/ManagerHome'));
+
+export const ManagerBuildingPage = lazy(() =>
+  import('./manager/buildings/ManagerBuildingPage')
+);
+export const ManagerBaseSurveyPage = lazy(() =>
+  import('./manager/surveys/ManagerBaseSurveyPage')
+);
