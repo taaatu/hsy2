6e8ad41e04e3136218d64bbe39929d975b9e7f0e
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-16 19:53:10 +0200

    image import

--- frontend/src/components/HsyLogo.tsx
+++ frontend/src/components/HsyLogo.tsx
@@ -1,4 +1,4 @@
-import logo from '../assets/hsy_icon.webp';
+const logo = new URL('../assets/hsy_icon.webp', import.meta.url).href;
 import styles from './Components.module.css';
 
 export const HsyLogo = () => {
