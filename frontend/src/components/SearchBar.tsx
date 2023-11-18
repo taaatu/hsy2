e11e7c7580cf2f4f90ad755400220de23874b7b0
Tatu <tatu.d.ihaksi@gmail.com>
2023-11-18 15:38:09 +0200

    search bar width fixed

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -7,10 +7,7 @@ type Props = {
 };
 
 export const SearchBar = ({ placeholder, handleSearch }: Props) => (
-  <div
-    className="color3 rounded"
-    style={{ position: 'relative', maxWidth: '100%' }}
-  >
+  <div className={`color3 rounded ${styles.searchBar}`}>
     <FaSearch className={styles.searchIcon} />
     <input
       className="search-bar line"
